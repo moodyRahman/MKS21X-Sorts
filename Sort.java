44a4f4b9881d83f8d22cf9b0104965e82cafce4a
Moody <mrahman01@stuy.edu>
2018-12-04 21:43:38 -0500

    basic "what i think it should do without testing it lmao"

---
+++ Sort.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Sort{
+	public static void selectionSort(int[] ary){
+		int index = 0;
+		int smallest = 0;
+		int[] output = new int[ary.length];
+		while (index < ary.length){
+			for (int x = index; x < ary.length; x++){
+				if (ary[x] < smallest){
+					smallest = ary[x];
+				}
+			}
+			output[index] = smallest;
+			index++;
+		}
+
+		for(int x = 0; x < output.length; x++){
+			System.out.println(output[x]);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] test = new int[100];
+		for (int x = 0; x < 101; x++){
+
+		}
+	}
+}
