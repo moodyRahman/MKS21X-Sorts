2d2cf33416e46052d9266b57f56c31ec3dc5a863
Moody <mrahman01@stuy.edu>
2018-12-04 22:41:10 -0500

    looks promising, tried and tested "smallest number" algorithm

--- Sort.java
+++ Sort.java
@@ -2,7 +2,26 @@ import java.util.Random;
 
 public class Sort{
 	public static void selectionSort(int[] ary){
-		
+		int index = 0;
+		int[] output = new int[ary.length];
+		int smallest = ary[0];
+
+		while (index < ary.length){
+			for(int x = index; x < ary.length; x++){
+				if (ary[x] < smallest){
+					System.out.print(ary[x] + ", " + smallest);
+					System.out.println();
+					smallest = ary[x];
+				}
+			}
+			output[index] = smallest;
+			index++;
+		}
+
+		System.out.println("aaaaa");
+		for (int x : output){
+			System.out.println(x);
+		}
 	}
 
 	public static void main(String[] args) {
