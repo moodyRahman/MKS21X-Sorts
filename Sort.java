bb2961003be3fdee08f0393315e1b2b90fa83057
Moody <mrahman01@stuy.edu>
2018-12-04 22:17:00 -0500

    code nukage lmao, something fundamentally wrong ig

--- Sort.java
+++ Sort.java
@@ -2,19 +2,13 @@ import java.util.Random;
 
 public class Sort{
 	public static void selectionSort(int[] ary){
-		int index = 0;
-		int smallest = 0;
-		int[] output = new int[ary.length];
-		while (index < ary.length){
-			for (int x = index; x < ary.length; x++){
-				if (ary[x] < smallest){
-					smallest = ary[x];
-				}
-			}
-			output[index] = smallest;
-			index++;
+		for(int x = 0; x < ary.length; x++){
+			System.out.println(ary[x]);
 		}
 
+		System.out.println("AAAAAAA");
+
+		System.out.println("BBBBB");
 		for(int x = 0; x < output.length; x++){
 			System.out.println(output[x]);
 		}
@@ -22,9 +16,9 @@ public class Sort{
 
 	public static void main(String[] args) {
 		Random randgen = new Random();
-		int[] test = new int[100];
-		for (int x = 0; x < 101; x++){
-			test[x] = randgen.nextInt();
+		int[] test = new int[20];
+		for (int x = 0; x < 20; x++){
+			test[x] = randgen.nextInt() % 10;
 		}
 
 		selectionSort(test);
