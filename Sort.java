30ed99bc89b14eb4c33a9cd2ec4871cc2961d578
Moody <mrahman01@stuy.edu>
2018-12-04 23:06:12 -0500

    WORKS! the issue was that the calculated smallest number in ary was not being accounted for by removal or altering into a really big number

--- Sort.java
+++ Sort.java
@@ -2,22 +2,32 @@ import java.util.Random;
 
 public class Sort{
 	public static void selectionSort(int[] ary){
+		for (int x : ary){
+			System.out.println(x);
+		}
+		
 		int index = 0;
 		int[] output = new int[ary.length];
+		int smallPos = 0;
+		int largest = ary[0];
+
+		for (int x = 0; x < ary.length; x++){
+			if (ary[x] > largest){
+				largest = ary[x];
+			}
+		}
 
 		while (index < ary.length){
 			int smallest = ary[index];
 			for (int x = 0; x < ary.length; x++){
 				if (ary[x] < smallest){
 					smallest = ary[x];
+					smallPos = x;
 				}
 			}
+			ary[smallPos] = largest + 1;
 			output[index] = smallest;
 			index++;
-		}
-
-		for (int x : ary){
-			System.out.println(x);
 		}
 
 		System.out.println("aaaaa");
