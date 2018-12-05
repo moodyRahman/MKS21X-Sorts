b97412cf3a07e1dc85b44a439403da2a7c00f0a1
Moody <mrahman01@stuy.edu>
2018-12-05 09:18:35 -0500

    removed printing, added algorithm timer

--- Sort.java
+++ Sort.java
@@ -2,10 +2,10 @@ import java.util.Random;
 
 public class Sort{
 	public static void selectionSort(int[] ary){
-		for (int x : ary){
-			System.out.println(x);
-		}
-		
+		// for (int x : ary){
+		// 	System.out.println(x);
+		// }
+
 		int index = 0;
 		int[] output = new int[ary.length];
 		int smallPos = 0;
@@ -30,19 +30,23 @@ public class Sort{
 			index++;
 		}
 
-		System.out.println("aaaaa");
-		for (int x : output){
-			System.out.println(x);
-		}
+		// System.out.println("aaaaa");
+		// for (int x : output){
+		// 	System.out.println(x);
+		// }
 	}
 
 	public static void main(String[] args) {
 		Random randgen = new Random();
-		int[] test = new int[20];
-		for (int x = 0; x < 20; x++){
-			test[x] = randgen.nextInt() % 10;
+		int[] test = new int[50000];
+		for (int x = 0; x < 50000; x++){
+			test[x] = randgen.nextInt() % 5000000;
 		}
 
+		long startTime = System.nanoTime();
 		selectionSort(test);
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);
+		System.out.println(duration / 1000000);
 	}
 }
