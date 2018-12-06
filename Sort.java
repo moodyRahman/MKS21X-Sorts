a6caae114166d12e319e962f381ef1653fb54988
Moody <mrahman01@stuy.edu>
2018-12-06 08:54:21 -0500

    two hours worth of work and and five sheets of notebook paper gone into this function// forgot to commit last night :(

--- Sort.java
+++ Sort.java
@@ -2,51 +2,45 @@ import java.util.Random;
 
 public class Sort{
 	public static void selectionSort(int[] ary){
-		// for (int x : ary){
-		// 	System.out.println(x);
-		// }
-
-		int index = 0;
-		int[] output = new int[ary.length];
-		int smallPos = 0;
-		int largest = ary[0];
-
-		for (int x = 0; x < ary.length; x++){
-			if (ary[x] > largest){
-				largest = ary[x];
-			}
-		}
-
-		while (index < ary.length){
-			int smallest = ary[index];
-			for (int x = 0; x < ary.length; x++){
-				if (ary[x] < smallest){
-					smallest = ary[x];
-					smallPos = x;
+		int length = ary.length;
+		for (int x = 0; x < length - 1; x++){
+			for (int val = x + 1; val < length; val++){
+				if (ary[x] > ary[val]){
+					int temp = ary[x];
+					ary[x] = ary[val];
+					ary[val] = temp;
+					for (int b : ary){
+						System.out.print(b);
+						System.out.print(", ");
+					}
+					System.out.println();
 				}
 			}
-			ary[smallPos] = largest + 1;
-			output[index] = smallest;
-			index++;
 		}
 
-		// System.out.println("aaaaa");
-		// for (int x : output){
-		// 	System.out.println(x);
-		// }
+		for (int x : ary){
+			System.out.print(x);
+			System.out.print(", ");
+		}
 	}
 
 	public static void main(String[] args) {
 		Random randgen = new Random();
-		int[] test = new int[50000];
-		for (int x = 0; x < 50000; x++){
-			test[x] = randgen.nextInt() % 5000000;
+		int[] test = new int[20];
+		for (int x = 0; x < 20; x++){
+			test[x] = randgen.nextInt() % 9;
 		}
 
-		long startTime = System.nanoTime();
+		System.out.print("OG LIST \n");
+		for (int x: test){
+			System.out.print(x + ", ");
+		}
+		System.out.println("\nEND OG");
+
+		// long startTime = System.nanoTime();
 		selectionSort(test);
-		long endTime = System.nanoTime();
-		long duration = (endTime - startTime);
-		System.out.println(duration / 1000000);
+		// long endTime = System.nanoTime();
+		// long duration = (endTime - startTime);
+		// System.out.println(duration / 1000000);
 	}
 }
